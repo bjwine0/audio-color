1443d92956eb91fbba5d21aac02a96c67f868355
Adrienne <adrienneeaston@gmail.com>
2019-08-19 21:08:17 -0700

    server running locally

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
+const faker = require('faker');
+
+setInterval( () => {
+  socket.emit('speak', faker.hacker.phrase());
+}, 750); 
